610e224b6241effeddc3d639de8d1a777674087d
Daksh Gupta <dakshkumargupta@gmail.com>
2017-06-13 14:33:09 -0500

    Created server

---
+++ server.py
@@ -0,0 +1,44 @@
+from main import getTranscript, createDocx
+from bottle import Bottle, route, get, request, run, static_file, error, redirect
+import sys
+
+capio = Bottle()
+
+@capio.route('/')
+def default():
+	return
+
+@capio.get('/transcript/<transcriptID>')
+def executeMain(transcriptID):
+	APIKey = request.headers.get('apiKey')
+	transcript = getTranscript(transcriptID, APIKey)
+	if type(transcript) is str:
+		print(transcript)
+		return transcript
+
+	elif len(transcript) > 0:
+		doc, path = createDocx(transcript, transcriptID)
+		if doc is None:
+			print(path)
+			return 'Unable to create file'
+
+		else:
+			print('File saved at: {}'.format(path))
+			path = path.split('{}.docx'.format(transcriptID))[0]
+			print(path)
+			return static_file('{}.docx'.format(transcriptID), root=path, download='{}.docx'.format(transcriptID))
+
+
+if __name__ == '__main__':
+	if len(sys.argv) == 3:
+		ip = sys.argv[1]
+		
+		try:
+			assert sys.argv[2].isdigit()
+		except AssertionError:
+			print('\nUsage:\n')
+			print('python server.py <IP> <PORT_integer>')
+			exit()
+
+		port = int(sys.argv[2])
+		run(capio, host=ip, port=port, reloader=True)
